f55cb689c47ddfb886f42b20cee561ad17bf9b48
kpendley <115032118+kpendley@users.noreply.github.com>
2022-11-08 12:03:50 -0700

    Update Internetindicator.py

--- Internetindicator.py
+++ Internetindicator.py
@@ -1,24 +1,32 @@
-#!/bin/python3
+#!/bin/python
 
-from gpiozero import LED, PingServer, Button
+from gpiozero import LED, PingServer, Button, PWMLED
 from gpiozero.tools import negated
-from signal import pause
+from time import sleep
 
 button = Button(26)
+
 clear = LED(2)
 green = LED(3)
-yellow = LED(4)
+yellow = PWMLED(4)
 red = LED(17)
 
-while true:
-        button.when_pressed()
 
-        print "Connection inquired"
+while True:
+        clear.value = 1 # full brightness
+
+        yellow.value = 0 # off
+        sleep(.2)
+        yellow.value = 0.5 # half brightness
+        sleep(.2)
+        yellow.value = 1 # full brightness
+
+        button.when_pressed(26)
+        yellow.value = 0 # off
+        print("Connection inquired")
 
         google = PingServer('google.com')
 
         google.when_activated = green.on
         google.when_deactivated = green.off
         red.source = negated(green)
-
-pause()
